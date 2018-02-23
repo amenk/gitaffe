affea23265d82812a4053d6cea4a1cc081f20581
Alexander Menk <a.menk@imi.de>
2018-02-23 22:14:47 +0100

    Use last commit message

--- gitaffe.sh
+++ gitaffe.sh
@@ -1,8 +1,9 @@
+message=`git log -1 --pretty=%B"`
 subhash=false
 current_ts=$(date -u +%s)
 while [ "$subhash" != "affe"  ]
 do
-	GIT_COMMITTER_DATE=date -d @$current_ts git commit --amend -m "monkey=$i" > /dev/null
+	GIT_COMMITTER_DATE=`date -d @$current_ts` git commit --amend -m "$message" > /dev/null
 	current_ts=$[$current_ts+1]
 	hash=`git rev-parse HEAD`
 	subhash=${hash:0:4}
